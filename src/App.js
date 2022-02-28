474ed3196678d8cd56f01064dec73a8d5f766d80
avendo <namankaushikplt@gmail.com>
2022-02-28 16:30:18 +0530

    - Fixed the problem from commit #124f093 (App did not open)  - Added the form and filterbutton to App.js  - Dynamic filter button(Added names)

--- src/App.js
+++ src/App.js
@@ -9,39 +9,11 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form/>
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton name="Active"/>
+        <FilterButton name="All"/>
+        <FilterButton name="Completed"/>
       </div>
       <h2 id="list-heading">
         3 tasks remaining
@@ -99,10 +71,10 @@ function App(props) {
             </button>
           </div>
         </li> */}
-        todoComponentList
+        {todoComponentList}
       </ul>
     </div>
   );
 }
 
-export default App();
+export default App;
